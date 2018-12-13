833a6af5fe3c97e2980fbcfc3370d3abea040fba
Lucas Cardena <lcardena9@gmail.com>
2018-12-13 15:54:34 -0800

    first push

---
+++ precache-manifest.3118d69d6e7a44c44d2c20874735ed53.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ff79201e207e5e18bce9c0958f29209",
+    "url": "/static/media/carneAsada.5ff79201.jpg"
+  },
+  {
+    "revision": "725b7bed337a975bd7a4",
+    "url": "/static/css/main.553c341e.chunk.css"
+  },
+  {
+    "revision": "6f575bcdf7b3dffa0d78",
+    "url": "/static/js/1.6f575bcd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c25640efd81dde12a692b1b89aa536a",
+    "url": "/static/media/tempBus.4c25640e.jpg"
+  },
+  {
+    "revision": "bd24680b642d3dbf8db072634495035c",
+    "url": "/static/media/tacoCornBeans.bd24680b.jpg"
+  },
+  {
+    "revision": "53b4ec41357d2c31a0cdebef2a7e69e7",
+    "url": "/static/media/party.53b4ec41.jpeg"
+  },
+  {
+    "revision": "fc63a529177601b74cdb0db64ea8b3b7",
+    "url": "/static/media/19thlogo.fc63a529.png"
+  },
+  {
+    "revision": "bd39787897d76485f21b7919399550e9",
+    "url": "/static/media/tempBus2.bd397878.jpg"
+  },
+  {
+    "revision": "417034e440812f799c3d016244782177",
+    "url": "/static/media/carnitas.417034e4.jpg"
+  },
+  {
+    "revision": "f4c4f894495d215c683aa0cd73d4b96a",
+    "url": "/static/media/soyRizo.f4c4f894.jpg"
+  },
+  {
+    "revision": "725b7bed337a975bd7a4",
+    "url": "/static/js/main.725b7bed.chunk.js"
+  },
+  {
+    "revision": "dd66e17c0e3e8adad31127c3781eee20",
+    "url": "/static/media/churros.dd66e17c.jpg"
+  },
+  {
+    "revision": "49b1491e51196d429dbf58f5d0ddc31f",
+    "url": "/static/media/elote.49b1491e.jpg"
+  },
+  {
+    "revision": "1e30b87392517aaa8122093d47c2765e",
+    "url": "/static/media/quesadillas.1e30b873.jpg"
+  },
+  {
+    "revision": "f38d7f2081065db55d9e751770365610",
+    "url": "/static/media/polloAsado.f38d7f20.jpg"
+  },
+  {
+    "revision": "101ca4115f08dd67fec1c7d4d61b322a",
+    "url": "/static/media/tacos.101ca411.jpg"
+  },
+  {
+    "revision": "d5130aeae1957ed770168c2010ebea88",
+    "url": "/static/media/salsas.d5130aea.jpg"
+  },
+  {
+    "revision": "6e2197c17b609a1efe5428bcd506e928",
+    "url": "/static/media/19thlogoText.6e2197c1.png"
+  },
+  {
+    "revision": "83ba08afb2a60b09262158c6f1bb6167",
+    "url": "/static/media/homeTacos.83ba08af.jpg"
+  },
+  {
+    "revision": "21935dd1ff21e0d801da290d1ed02598",
+    "url": "/static/media/tacosBanner.21935dd1.jpg"
+  },
+  {
+    "revision": "f86948a869218f228d1968104fd2ca68",
+    "url": "/index.html"
+  }
+];
